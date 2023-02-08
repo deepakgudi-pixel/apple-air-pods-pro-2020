002a56cea1f8392318d71f7649dc2b3e12ce4764
deepakgudi-pixel <japanesen52018@gmail.com>
2023-02-09 01:33:10 +0530

    main nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./utils/styles/App.scss";
 
 import NavbarOne from "./components/navbar/NavbarOne";
+import NavbarTwo from "./components/navbar/NavbarTwo";
 
 
 export default function App() {
   return (
     <>
       <NavbarOne />
+      <NavbarTwo/>
     </>
   );
 }
---
+++ src/components/navbar/NavbarTwo.jsx
@@ -0,0 +1,43 @@
+export default function NavbarTwo() {
+  return (
+    <nav className="sec__nav">
+      <div className="sec__nav--container">
+        <div className="sec__nav--content">
+          <div className="sec__nav--title">
+            <a href="#" className="airpods">
+              AirPods&nbsp;Pro
+              <br className="break" />
+              (2nd generation)
+            </a>
+          </div>
+          <div className="sec__nav--menu">
+            <div className="menu__tray">
+              <ul className="menu__items">
+                <li className="menu__item">
+                  <a className="menu__links" href="#">
+                    Overview
+                  </a>
+                </li>
+                <li className="menu__item">
+                  <a className="menu__links" href="#">
+                    Tech Specs
+                  </a>
+                </li>
+                <li className="menu__item">
+                  <a className="menu__links" href="#">
+                    Compare
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="sec__nav--menu-cta">
+              <a className="cta__btn" href="#">
+                Buy
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
